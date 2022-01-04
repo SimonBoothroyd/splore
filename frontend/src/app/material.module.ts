bad15cf234a1398e46805d2445b773cfef673478
SimonBoothroyd <simon.boothroyd@hotmail.com>
2022-01-04 11:55:38 +0000

    Fix header position Remove redundant css and material modules

--- frontend/src/app/material.module.ts
+++ frontend/src/app/material.module.ts
@@ -3,11 +3,9 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatDialogModule } from "@angular/material/dialog";
 import { MatDividerModule } from "@angular/material/divider";
 import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatGridListModule } from "@angular/material/grid-list";
 import { MatIconModule } from "@angular/material/icon";
 import { MatInputModule } from "@angular/material/input";
 import { MatMenuModule } from "@angular/material/menu";
-import { MatPaginatorModule } from "@angular/material/paginator";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatTooltipModule } from "@angular/material/tooltip";
@@ -18,11 +16,9 @@ import { MatTooltipModule } from "@angular/material/tooltip";
     MatDialogModule,
     MatDividerModule,
     MatFormFieldModule,
-    MatGridListModule,
     MatIconModule,
     MatInputModule,
     MatMenuModule,
-    MatPaginatorModule,
     MatProgressSpinnerModule,
     MatToolbarModule,
     MatTooltipModule,
